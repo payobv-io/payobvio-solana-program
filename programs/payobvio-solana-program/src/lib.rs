d6303b965dd1eb448b7a2522025a6b1b2b529404
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-08-19 14:05:43 +0900

    Added: A basic smart contract program to store a message

--- programs/payobvio-solana-program/src/lib.rs
+++ programs/payobvio-solana-program/src/lib.rs
@@ -6,11 +6,31 @@ declare_id!("FQvAzGRC6eSDaa1GvuXRDnbBxAxCT8WhwLsj8psnWxSk");
 pub mod payobvio_solana_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, message: String) -> Result<()> {
+        let account = &mut ctx.accounts.data_account;
+        account.message = message;
         msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Message: {:?}", account.message);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 40,
+        seeds = [b"data_account".as_ref(), user.key().as_ref()],
+        bump
+    )]
+    pub data_account: Account<'info, DataAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct DataAccount {
+    pub message: String,
+}
