0d591ab8e64fd5d79b6e775c9c77058c5566dac5
Muhammed Ayimen <aiymenlatheef03@gmail.com>
2024-08-21 13:40:38 +0900

    Added: Initialize escrow instruction along with escrow states enum

--- programs/payobvio-solana-program/src/lib.rs
+++ programs/payobvio-solana-program/src/lib.rs
@@ -1,36 +1,56 @@
 use anchor_lang::prelude::*;
 
-declare_id!("FQvAzGRC6eSDaa1GvuXRDnbBxAxCT8WhwLsj8psnWxSk");
+declare_id!("Fu4phdrbvax4SPaQPBcurJFX6YtiqrJLzgh6ryacnwpJ");
 
 #[program]
 pub mod payobvio_solana_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, message: String) -> Result<()> {
-        let account = &mut ctx.accounts.data_account;
-        account.message = message;
-        msg!("Greetings from: {:?}", ctx.program_id);
-        msg!("Message: {:?}", account.message);
+    pub fn initialize_escrow(
+        ctx: Context<InitializeEscrow>,
+        bounty_amount: u64,
+        issue_id: String,
+    ) -> Result<()> {
+        let escrow_account = &mut ctx.accounts.escrow_account;
+        escrow_account.maintainer = ctx.accounts.maintainer.key();
+        escrow_account.amount = bounty_amount;
+        escrow_account.issue_id = issue_id;
+        escrow_account.contributor = Pubkey::default();
+        escrow_account.state = EscrowState::Initialized;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
+#[instruction(bounty_amount: u64, issue_id: String)]
+pub struct InitializeEscrow<'info> {
+    #[account(mut)]
+    pub maintainer: Signer<'info>,
     #[account(
         init,
-        payer = user,
-        space = 8 + 40,
-        seeds = [b"data_account".as_ref(), user.key().as_ref()],
+        payer = maintainer,
+        space = 8 + 32 + 32 + 8 + 32 + 1 + issue_id.len(),
+        seeds = [b"escrow", issue_id.as_bytes()],
         bump
     )]
-    pub data_account: Account<'info, DataAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
+    pub escrow_account: Account<'info, EscrowAccount>,
     pub system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct DataAccount {
-    pub message: String,
+pub struct EscrowAccount {
+    pub maintainer: Pubkey,
+    pub contributor: Pubkey,
+    pub amount: u64,
+    pub issue_id: String,
+    pub state: EscrowState,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+pub enum EscrowState {
+    Initialized,
+    Funded,
+    Assigned,
+    Completed,
+    Refunded,
 }
